3e1d974db3393e795ee78b9eda5b499993b312c5
eversometiculous <126760003+eversometiculous@users.noreply.github.com>
2023-08-26 10:02:03 +1000

    Added npm init, jest, created diceRolling.test.js and diceRolling.test

---
+++ README.md
@@ -0,0 +1,13 @@
+# feb23-t3w2-jest
+
+# Dice rolling app
+
+## Users
+
+- Tabletop RPG gamers
+    - Roll a variety of dice and get results
+    - Roll a twenty-sided dice (D20) and see the result
+    - Roll a twenty-sided dice (D20) twice and see the worst result (roll with disadvantage)
+    - Roll a twenty-sided dice (D20) twice and see the best result (roll with advantage)
+- Casual tabletop gamers
+    - Roll a six-sided dice (D6) and see the result
---
+++ tests/diceRolling.test.js
@@ -0,0 +1,37 @@
+const {rollDice} = require(.../src/diceRolling);
+
+describe("Casual tabletop gamers wants to...", () =>{
+
+    test("...wants to roll a D6 and see a valid result.", () => {      // or you can run xtest("...wants to roll a D6 and see a valid result.", () => {
+        expect(rollDice)
+    });
+
+});
+
+describe("Tabletop RPG gamer...", () => {
+    test("...wants to roll a D20 with advantage and see both roll results as well as the best roll.", () => {
+
+    });
+
+    test("...wants to roll a D20 with disadvantage and see both roll results as well as the worst roll.", () => {
+
+    });
+
+    // In D&D, we have these dice:
+    // D4, D6, D8, D12, D20, D100
+
+    describe("...wants to roll a variety of dice. Specfically, they want to roll a ...", () => {
+		test("D4", () => {
+
+		});
+
+		test("D6", () => {
+
+		});
+
+		test("D8", () => {
+
+		});
+    });
+
+});
