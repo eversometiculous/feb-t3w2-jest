a79770cecec5bffa7dc7fb19aad91c2c95891c36
eversometiculous <126760003+eversometiculous@users.noreply.github.com>
2023-08-29 19:26:01 +1000

    Added new code

--- src/diceRolling.js
+++ src/diceRolling.js
@@ -1,9 +1,62 @@
+require('dotenv').config();
 
+
+console.log("Current node environment is: \n" + process.env.NODE_ENV);
+console.log("Cool message is \n" + process.env.COOL_MESSAGE);
 
 function rollDice(diceSize = 6) {
 
+    // Math.floor always round down, so diceSzie of 6 would be 0-5
+    // Add 1 to guarantee that it stats within sensible range,
+    // eg. diceSize of 6 means possible results are 1-6
+
+    let diceTotal = Math.floor(Math.random() * diceSize) + 1; 
+
+    return diceTotal;
+}
+
+function rollDiceWithDisadvantage(diceSize = 6){
+	let result = {
+		finalResult: null,
+		rolls: []
+	}
+
+    result.rolls = [
+        rollDice(diceSize),
+        rollDice(diceSize)
+    ];
+    //let someArray = [1, 2, 3, 4]
+
+
+    // Copy result.Rolls into a new array that only exists for Math.min() to analyze
+    result.finalResult = Math.min(...result.rolls);
+
+
+	return result;
+}
+
+function rollDiceWithAdvantage(diceSize = 6){
+	let result = {
+		finalResult: null,
+		rolls: []
+	}
+
+    result.rolls = [
+        rollDice(diceSize),
+        rollDice(diceSize)
+    ];
+    //let someArray = [1, 2, 3, 4]
+
+
+    // Copy result.Rolls into a new array that only exists for Math.max() to analyze
+    result.finalResult = Math.max(...result.rolls);
+
+
+	return result;
 }
 
 module.exports = {
-    rollDice
+    rollDice,
+    rollDiceWithDisadvantage,
+    rollDiceWithAdvantage
 }
--- tests/diceRolling.test.js
+++ tests/diceRolling.test.js
@@ -1,9 +1,24 @@
-const {rollDice} = require(.../src/diceRolling);
+const {rollDice, rollDiceWithDisadvantage, rollDiceWithAdvantage} = require('../src/diceRolling');
+
+//beforeEach(() => {
+//    console.log("message runs before each individual test()");
+//})
+
+//afterEach(() => {
+//    console.log("message runs AFTER each individual test()");
+// })
 
 describe("Casual tabletop gamers wants to...", () =>{
 
-    test("...wants to roll a D6 and see a valid result.", () => {      // or you can run xtest("...wants to roll a D6 and see a valid result.", () => {
-        expect(rollDice)
+    // xtest("...wants to roll a D6 and see a valid result.", () => {
+    // });
+
+    test("...wants to roll a D6 and see a valid result.", () => {
+
+        let diceResult = rollDice();
+
+        expect(diceResult).toBeGreaterThan(0);
+        expect(diceResult).toBeLessThanOrEqual(6);
     });
 
 });
@@ -11,27 +26,114 @@ describe("Casual tabletop gamers wants to...", () =>{
 describe("Tabletop RPG gamer...", () => {
     test("...wants to roll a D20 with advantage and see both roll results as well as the best roll.", () => {
 
+        let rollResult = rollDiceWithAdvantage(20);
+//        console.log("rollResult = \n" + rollResult.finalResult + ", " + rollResult.rolls); or you can do
+        console.log("Roll result: \n" + JSON.stringify(rollResult, null, 4));
+
+        expect(rollResult.finalResult).toBeGreaterThan(0);
+        expect(rollResult.finalResult).toBeLessThanOrEqual(20);
+
+        expect(rollResult.rolls).toHaveLength(2);
+        expect(rollResult.rolls).toContain(rollResult.finalResult);
+        let sortedRolls = [...rollResult.rolls].sort(a, b) => a - b;
+        // expect(sortedRolls[0] > sortedRolls[1]).toBe(true);
+
     });
+
+//    let rollDiceWithDisadvantage = jest.fn().mockReturnValue({
+//        finalResult: 1,
+//        rolls: [
+//            1, 20
+//        ]
+//    });
 
     test("...wants to roll a D20 with disadvantage and see both roll results as well as the worst roll.", () => {
 
+        let rollResult = rollDiceWithDisadvantage(20);
+//        console.log("rollResult = \n" + rollResult.finalResult + ", " + rollResult.rolls); or you can do
+        console.log("Roll result: \n" + JSON.stringify(rollResult, null, 4));
+
+        expect(rollResult.finalResult).toBeGreaterThan(0);
+        expect(rollResult.finalResult).toBeLessThanOrEqual(20);
+
+        expect(rollResult.rolls).toHaveLength(2);
+        expect(rollResult.rolls).toContain(rollResult.finalResult);
+    });
+
+
+    test("...wants to roll a D20 one hundred times and see how many natural 20s they get.", () => {
+        let diceResults = [];
+        for (let index = 0; index < 100; index ++) {
+            diceResults.push(rollDice(20));
+        }
+
+        console.log(diceResults);
+
+        expect(diceResults).toContain(20);
+        // expect(diceResults.length).toEqual(100); // this line is the same as below
+        expect(diceResults).toHaveLength(100);
     });
 
     // In D&D, we have these dice:
     // D4, D6, D8, D12, D20, D100
 
     describe("...wants to roll a variety of dice. Specfically, they want to roll a ...", () => {
-		test("D4", () => {
 
-		});
+        let diceVarieties = [
+            {
+                diceSize: 4,
+                diceName: "D4"
+            },
+            {
+                diceSize: 6,
+                diceName: "D6"
+            },
+            {
+                diceSize: 8,
+                diceName: "D8"
+            },
+            {
+                diceSize: 10,
+                diceName: "D10"
+            },
+            {
+                diceSize: 12,
+                diceName: "D12"
+            },
+            {
+                diceSize: 20,
+                diceName: "D20"
+            },
+            {
+                diceSize: 100,
+                diceName: "D100"
+            },
+        ]
 
-		test("D6", () => {
+        test.each(diceVarieties)("$diceName", (diceObj) => {
+            let diceResult = rollDice(diceObj.diceSize);
+            expect(diceResult).toBeGreaterThan(0);
+            expect(diceResult).toBeLessThanOrEqual(diceObj.diceSize);
+        })
 
-		});
+//		test("D4", () => {
+//            let diceResult = rollDice(4);
+//            expect(diceResult).toBeGreaterThan(0);
+//            expect(diceResult).toBeLessThanOrEqual(4);
+//		});
 
-		test("D8", () => {
+//		test("D6", () => {
+//            let diceResult = rollDice();
+//            // let diceResult = rollDice(6); equivalent to the line above
+//            expect(diceResult).toBeGreaterThan(0);
+//            expect(diceResult).toBeLessThanOrEqual(6);
+//		});
 
-		});
+//		test("D8", () => {
+//            let diceResult = rollDice(8);
+//            expect(diceResult).toBeGreaterThan(0);
+//            expect(diceResult).toBeLessThanOrEqual(8);
+//		});
     });
 
 });
